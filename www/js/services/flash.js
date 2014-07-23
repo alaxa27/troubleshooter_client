520dc1eea1839248f0dd675105121b454bc9750d
taqiy <alaxa27@gmail.com>
2014-07-23 17:45:12 +0200

    -removing index.js import -importing flash service

---
+++ www/js/services/flash.js
@@ -0,0 +1,34 @@
+troubleshooter.factory('FlashService', function ($rootScope, $timeout) {
+
+    var clear = function(){
+        $rootScope.rootData.flash = '';
+        $rootScope.rootData.loading = false;
+        return null;
+        };
+    return {
+        show: function (message, type, timeout) {
+            //Type can be: error, success, info
+            //Type is used by the view to display the proper color
+            var cssType;
+            if (type == "error" ) {
+                cssType = "assertive";
+            } else if ( type == "success" ) {
+                cssType = "balanced";
+            } else if ( type == "info" ) {
+                cssType = "positive";
+            } else {
+                console.log("Error: Wrong type in FlashService call.");
+            }
+            $rootScope.rootData.loading = true;
+            $rootScope.rootData.flash = {
+                                'message': message
+                                , 'type': cssType
+                                };
+            $timeout(function(){
+                clear();
+            }, timeout);
+            return message;
+        },
+     
+    };
+});
