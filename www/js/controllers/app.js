01c217d0079a5eda32b6794dbc2628f2eefe72b8
taqiy <alaxa27@gmail.com>
2014-07-23 18:16:04 +0200

    -Implementing data needed by the app in rootData ToDo: implement it in a service#
    Using FlashService wherever its needed

--- www/js/controllers/app.js
+++ www/js/controllers/app.js
@@ -1,13 +1,23 @@
-troubleshooter.controller('AppCtrl', function($scope, $rootScope, $ionicModal, $timeout) {
-  $rootScope.apiUrl = "http://localhost:1337/api"
+troubleshooter.controller('AppCtrl', function($scope, $rootScope, $ionicModal, $timeout, FlashService) {
+  $rootScope.rootData = {
+    'apiUrl' : "http://localhost:1337/api"
+    , 'flash': ''
+    , 'Modal': {
+        'isShown': false
+    }
+  };
   // Form data for the login modal
   $scope.loginData = {};
-  $scope.currentUser = true;
+  $scope.currentUser = false;
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
     scope: $scope
   }).then(function(modal) {
     $scope.LoginModal = modal;
+  });
+  // When modal is closed
+  $scope.$on('modal.hidden', function() {
+    $rootScope.rootData.Modal.isShown = false;
   });
 
   // Triggered in the login modal to close it
@@ -18,6 +28,7 @@ troubleshooter.controller('AppCtrl', function($scope, $rootScope, $ionicModal, $
   // Open the login modal
   $scope.openLoginModal = function() {
     $scope.LoginModal.show();
+    $rootScope.rootData.Modal.isShown = true;
   };
 
   // Perform the login action when the user submits the login form
@@ -26,15 +37,25 @@ troubleshooter.controller('AppCtrl', function($scope, $rootScope, $ionicModal, $
 
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
-    $timeout(function() {
-      $scope.closeLoginModal();
-    }, 1000);
-    $scope.currentUser = true
+    if ($scope.loginData.username == $scope.loginData.password) {
+        $timeout(function() {
+          $scope.closeLoginModal();
+          $scope.loginData.username = '';
+          $scope.loginData.password = '';
+          FlashService.show("Succesfully logged in.", "success", 2000);
+        }, 1000);
+        $scope.currentUser = true
+    } else {
+        FlashService.show("Incorrect username/password", "error", 2000); 
+        $scope.loginData.username = '';
+        $scope.loginData.password = '';
+    }
   };
 
   $scope.doLogout = function() {
     $scope.currentUser = false;
+    FlashService.show("Successfully logged out.", "success", 2000);
   };
-})
+});
 
 
--- www/js/services/feeds.js
+++ www/js/services/feeds.js
@@ -1,4 +1,4 @@
 troubleshooter.factory('GETFeeds', function($resource, $rootScope) {
-    return $resource($rootScope.apiUrl + '/feeds');
+    return $resource($rootScope.rootData.apiUrl + '/feeds');
 });
 
